894e2379b291dee3624a97393d5921325553b563
harsh <bokharsh@gmail.com>
2021-07-11 00:28:09 +0530

    [V-1]: initial trials 0.1

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import { link } from 'gastby';
+import Layout from '../components/layout';
+
+export default class AboutPage extends Component {
+	render() {
+		return (
+			<Layout pageTitle={'About'}>
+				<div> This is the about Section</div>
+			</Layout>
+		);
+	}
+}
